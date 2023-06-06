a7884392c67ff30d9f6e502d41ebe2aff23f58bf
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-06-06 15:21:05 -0400

    adds Linked Lists Questions

---
+++ I-Data Structures/2-Linked Lists/2.2-returnKthToLast.js
@@ -0,0 +1,5 @@
+/*
+Implement an algorithm to find the kth to last element of a singly linked list.
+
+Hints: #8, #25, #41, #67, #126
+*/
---
+++ I-Data Structures/2-Linked Lists/2.6-palindrome.js
@@ -0,0 +1,5 @@
+/*
+Implement a function to check if a linked list is a palindrome.
+
+Hints: #5, #13, #29, #61, #101
+*/
