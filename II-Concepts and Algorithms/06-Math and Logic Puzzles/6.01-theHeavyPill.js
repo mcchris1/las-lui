9488e59bb540e0013300e4bb25c92f257486bfd7
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-06-12 00:21:35 -0400

    adds leading zeroes

--- II-Concepts and Algorithms/06-Math and Logic Puzzles/6.01-theHeavyPill.js
+++ II-Concepts and Algorithms/06-Math and Logic Puzzles/6.01-theHeavyPill.js
@@ -1,5 +1,9 @@
 /*
 You have 20 bottles of pills. 19 bottles have 1.0 gram pills, but one has pills of weight 1.1 grams. Given a scale that provides an exact measurement, how would you find the heavy bottle? You can only use the scale once.
 
-Hints: #186, #252, #319, #387
+Hints:
+#186: You can only use the scale once. This means that all, or almost all, of the bottles must be used. They also must be handled in different ways or else you couldn't distinguish between them.
+#252: 
+#319:
+#387:
 */
