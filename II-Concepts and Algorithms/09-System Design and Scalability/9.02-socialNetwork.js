f7a3ddad5ae349ae5499f91005841b67d93eef28
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-06-16 09:27:26 -0400

    adds System Design and Scalability Hints

---
+++ II-Concepts and Algorithms/09-System Design and Scalability/9.02-socialNetwork.js
@@ -0,0 +1,9 @@
+/*
+How would you design the data structures for a very large social network like Facebook or LinkedIn? Describe how you would design an algorithm to show the shortest path between two people (e.g., Me -> Bob -> Susan -> Jason -> You).
+
+Hints:
+#270: To find the connection between two nodes, would it be better to do a breadth-first search or depth-first search? Why?
+#285: A breadth-first search is probably better. A depth-first search can wind up going on a long path, even though the shortest path is actually very short. Is there a modification to a breadth-first search that might be even faster?
+#304: Is there any way to search from both the source and destination? For what reason or in what case might this be faster?
+#321: Discuss how you might implement these algorithms and this system in the real world. What sort of optimizations might you make?
+*/
---
+++ II-Concepts and Algorithms/09-System Design and Scalability/9.03-webCrawler.js
@@ -0,0 +1,8 @@
+/*
+If you were designing a web crawler, how would you avoid getting into infinite loops? 
+
+Hints:
+#334: Think about how infinite loops might occur.
+#353: How would you detect the same page? What does this mean?
+#365: How do you define if two pages are the same? Is it the URLs? Is it the content? Both of these can be flawed. Why?
+*/
